2fb4e2f7786186fc26379b5919bbfa215c6282d6
Jonathon Reid <jonathon.reid@auspost.com.au>
2015-05-26 17:19:21 +1000

    Added classes and Forms string

---
+++ app/src/main/java/auspost/com/au/hackday/FormsListAdapter.java
@@ -0,0 +1,36 @@
+package auspost.com.au.hackday;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by reidj2 on 26/05/2015.
+ */
+public class FormsListAdapter extends BaseAdapter {
+
+    private TextView formName;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+//        textService
+        return null;
+    }
+}
